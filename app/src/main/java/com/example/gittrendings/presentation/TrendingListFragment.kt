3ea499a401dacba8112b415a86c4b4c319df2192
anumkhan-airlift <91872998+anumkhan-airlift@users.noreply.github.com>
2022-07-25 02:57:52 +0500

    refresh and change mode feature added.

--- app/src/main/java/com/example/gittrendings/presentation/TrendingListFragment.kt
+++ app/src/main/java/com/example/gittrendings/presentation/TrendingListFragment.kt
@@ -1,10 +1,13 @@
 package com.example.gittrendings.presentation
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.*
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.Navigation
+import com.example.gittrendings.R
 import com.example.gittrendings.databinding.FragmentTrendingListBinding
 import com.example.gittrendings.network.TrendingUIState
 import dagger.hilt.android.AndroidEntryPoint
@@ -21,6 +24,7 @@ class TrendingListFragment: Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+        setHasOptionsMenu(true)
         binding = FragmentTrendingListBinding.inflate(inflater, container, false).apply {
             lifecycleOwner = viewLifecycleOwner
             viewModel = listViewModel
@@ -29,6 +33,22 @@ class TrendingListFragment: Fragment() {
             }
         }
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.refresh_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_refresh -> {
+                listViewModel.fetchTrendingProjects()
+            }
+            R.id.action_change_mode -> {
+                changeMode()
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -70,4 +90,17 @@ class TrendingListFragment: Fragment() {
                 )
         }
     }
+
+    private fun changeMode() {
+        val mode =
+            if ((resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) ==
+                Configuration.UI_MODE_NIGHT_NO
+            ) {
+                AppCompatDelegate.MODE_NIGHT_YES
+            } else {
+                AppCompatDelegate.MODE_NIGHT_AUTO_BATTERY
+            }
+        AppCompatDelegate.setDefaultNightMode(mode)
+    }
+
 }
